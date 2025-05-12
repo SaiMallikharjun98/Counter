e5d5ed7cdd7073521bb858f0c100a140fc9b4338
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-05-12 15:01:51 +0530

    Styles Updated commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -14,29 +14,29 @@ function Counter() {
 
   return (
     <>
-      <div className="w-full mx-auto">
-        <h1 className="text-2xl text-bold">This is a Simple Counter App</h1>
+      <div className="w-full max-w-md mx-auto p-4">
+        <h1 className="text-2xl font-bold">This is a Simple Counter App</h1>
         <p className="text-xl">
           The Counter App is a basic web application that allows users to
           increase, decrease, or reset a number displayed on the screen.
         </p>
-        <h2 className="text-4xl">{counter}</h2>
+        <h2 className="text-4xl text-center">{counter}</h2>
         <div className="flex gap-2">
           <button
             onClick={incrementHandler}
-            className="bg-green-600 text-xs text-white w-20 rounded-md"
+            className="bg-green-600 hover:bg-green-700 text-xs text-white w-28 rounded-md cursor-pointer"
           >
             Increment
           </button>
           <button
             onClick={resetHandler}
-            className="bg-violet-500 text-xs text-white w-15 rounded-md"
+            className="bg-violet-500 hover:bg-violet-700 text-xs text-white w-28 rounded-md cursor-pointer"
           >
             Reset
           </button>
           <button
             onClick={decrementHandler}
-            className="bg-red-500 text-xs text-white w-20 rounded-md"
+            className="bg-red-500 hover:bg-red-700 text-xs text-white w-28 rounded-md cursor-pointer"
           >
             Decrement
           </button>
